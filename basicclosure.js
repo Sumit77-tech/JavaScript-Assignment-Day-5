15995996cfcfc4abb578ae33d655eb4e6ee7fa86
SUMIT gourav <sumit.gourav280800@gmail.com>
2024-10-28 10:59:49 -0700

    first commit

---
+++ basicclosure.js
@@ -0,0 +1,11 @@
+function outerFunction() {
+    let message = "Hello, this is a closure example!";
+
+    function innerFunction() {
+        console.log(message);
+    }
+    return innerFunction;
+}
+
+const storedFunction = outerFunction();
+storedFunction();
